511cd1c534925db09f0ab462c4e242a0112971ed
Nunzio Tocci <nunzio@tocci.org>
2017-04-28 15:36:39 -0500

    make login_group envelope_g, bump version

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function spawnEnvelope(strAppName, callback) {
 			'-y', path.normalize(app.getAppPath() + '/app'),
 			//'-z', path.normalize(app.getAppPath() + '/envelope/role'),
 			'-x', 't',
-			'-g', '',
+			'-g', 'envelope_g',
 			(process.platform == 'win32' ? '-o' : ''), (process.platform == 'win32' ? 'stderr' : '')
 		], {
 			detached: true
